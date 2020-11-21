3f8b85840d3f9e6e6c88c1e05e0cd67c7b2648dc
Alexander <banderulez@ya.ru>
2020-11-21 21:38:04 +0300

    add homework3

--- js/main.js
+++ js/main.js
@@ -1,24 +1,69 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-];
-//Функция для формирования верстки каждого товара
-const renderProduct = (product, img='https://placehold.it/200x150') => {
-    return `<div class="product-item">
-                <img src="${img}" alt="img">
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+class ProductsList {
+    constructor(container ='.products') {
+    this.container = container;
+    this.goods = [];
+    this.allProducts = [];
+    this._getProducts()
+        .then(data => {
+            this.goods = [...data];
+            this.render()
+        });
+    // this._fetchProducrs();
+    }
+    // _fetchProducrs(){
+    //     this.goods = [
+    //         {id: 1, title: 'Notebook', price: 2000},
+    //         {id: 2, title: 'Mouse', price: 20},
+    //         {id: 3, title: 'Keyboard', price: 200},
+    //         {id: 4, title: 'Gamepad', price: 50},
+    //     ];
+    // }
+    _getProducts(){
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.innerHTML += productObject.render();
+        }
+    }
+    // calcPrice() {
+    //     let totalPrice = 0;
+    //     this.products.forEach((item) => {
+    //         totalPrice += item.price;
+    //     });
+    //     document.querySelector('.products-total').innerHTML = `Общая сумма товаров на ${totalPrice} рублей`;
+    // }
+    calcPrice() {
+        let res = this.allProducts.reduce((sum, item) => sum += item.price, 0);
+         document.querySelector('.products-total').innerHTML = `Общая сумма товаров на ${res} рублей`;
+    }
+}
+ class ProductItem {
+     constructor(product, img = 'https://placehold.it/200x150') {
+         this.title = product.product_name;
+         this.price = product.price;
+         this.id = product.id_product;
+         this.img = img;
+     }
+
+     render() {
+         return `<div class="product-item" >
+                <img src="${this.img}" alt="img">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
-};
-
-renderPage(products);
-
+     }
+ }
 
 class CartItem {
     constructor(title, price, img, link) {
@@ -30,16 +75,21 @@ class CartItem {
 
     }
 }
+
+
 class Cart {
     constructor() {
-
-        addToCart() {}
-
-        deleteFromCart() {}
-
-        calcCart() {}
-
-        render() {}
-
-        openCart() {}
+        addToCart();
+        deleteFromCart();
+        calcCart();
+        render();
+        openCart();
     }
+    render(){
+    }
+ }
+
+
+let list = new ProductsList();
+list.render();
+list.calcPrice();
